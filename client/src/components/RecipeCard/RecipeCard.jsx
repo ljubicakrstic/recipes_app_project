a356254648d047484c6942069238b302013ca173
ljubicakrstic <ljubica.z.krstic@gmail.com>
2020-09-26 19:06:30 +0200

    izmenjena ruta

--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -5,7 +5,7 @@ const RecipeCard = ({recipe}) => {
 
     return(
         <div className="card mb-4">
-            <img className="card-img-top" src={recipe.slika} alt={recipe.naziv}/>
+            <img className="card-img-top" src={recipe.slika} alt={recipe.naziv} />
             <div className="card-body">
                 <h2 className="card-title">{recipe.naziv}</h2>
                 <p className="card-text">{recipe.opis}</p>
